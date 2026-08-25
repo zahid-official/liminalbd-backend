9476099458d755cba765731b291c0b81194ce0c4
zahid-official <zahid.official8@gmail.com>
2026-08-25 11:51:43 +0600

    refactor(prisma): update configuration and streamline client initialization

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,5 @@
-/// <reference types="node" />
-
 import "dotenv/config";
-import { defineConfig } from "prisma/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -9,6 +7,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: env("DATABASE_URL"),
   },
 });
--- src/app/config/prisma.ts
+++ src/app/config/prisma.ts
@@ -1,15 +1,10 @@
+import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client.js";
-import env from "./env.js";
 
-// Database connection string
-const adapter = new PrismaPg({
-  connectionString: env.DATABASE_URL,
-});
-
-// Initialize Prisma client
-const prisma = new PrismaClient({
-  adapter,
-});
+// Prisma Client Configuration
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
